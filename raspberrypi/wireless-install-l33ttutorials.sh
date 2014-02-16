4d0e88e225d38fafc7ca12ff176339c9f41e9060
pablohenrique <pablo.haoko@gmail.com>
2014-02-16 15:40:02 +0000

    correction on path

--- raspberrypi/wireless-install-l33ttutorials.sh
+++ raspberrypi/wireless-install-l33ttutorials.sh
@@ -1,7 +1,9 @@
-!#/bin/bash
+#!/bin/bash
+
 wget https://dl.dropboxusercontent.com/u/80256631/8188eu-20131223.tar.gz
 tar -zxvf $(pwd)"/8188eu-20131223.tar.gz"
-sudo cp $(pwd)"rtl8188eufw.bin" /lib/firmware/rtlwifi
-sudo install -p -m 644 $(pwd)"8188eu.ko" "/lib/modules/"$(uname -r)"/kernel/drivers/net/wireless"
-sudo insmod $(pwd)"/lib/modules/"$(uname -r)"/kernel/drivers/net/wireless"
+
+sudo cp $(pwd)"/rtl8188eufw.bin" /lib/firmware/rtlwifi
+sudo install -p -m 644 $(pwd)"/8188eu.ko" "/lib/modules/"$(uname -r)"/kernel/drivers/net/wireless"
+sudo insmod "/lib/modules/"$(uname -r)"/kernel/drivers/net/wireless/8188eu.ko"
 sudo depmod -a
