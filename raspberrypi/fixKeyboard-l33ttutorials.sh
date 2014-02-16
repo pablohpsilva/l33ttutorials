8c5720275a20b07710be1cae1c985639afa67e52
pablohenrique <pablo.haoko@gmail.com>
2014-02-16 16:29:22 +0000

    rewriting keyboard file

--- raspberrypi/fixKeyboard-l33ttutorials.sh
+++ raspberrypi/fixKeyboard-l33ttutorials.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-sudo setxkbmap -layout us
-sudo setupcon
-sudo udevadm trigger --subsystem-match=input --action=change
+#!/bin/bash/
+echo "Rewriting   /etc/default/keyboard   file"
+
+echo '# KEYBOARD CONFIGURATION FILE''\n\n''# Consult the keyboard(5) manual page''\n\n''XKBMODEL="pc105"''\n''XKBLAYOUT="us"''\n''XKBVARIANT="altgr-intl"''\n''XKBOPTIONS=""''\n\n''BACKSPACE="guess"' > /etc/default/keyboard
