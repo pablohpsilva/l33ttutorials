ca0f2f67f09298f2909fe47a034223769c431a88
Pablo Henrique <pablo.haoko@gmail.com>
2014-06-05 10:55:44 -0400

    Update git-private-repository.sh

--- linux/git-private-repository.sh
+++ linux/git-private-repository.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y build-essential zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libncurses5-dev libffi-dev curl git-core openssh-server redis-server checkinstall libxml2-dev libxslt-dev libcurl4-openssl-dev libicu-dev
